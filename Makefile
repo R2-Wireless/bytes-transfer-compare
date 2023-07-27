b1a08a91788c4082d09abfd59d19438689804d8f
Yuval Saraf <unimonkiez@gmail.com>
2023-07-27 10:54:14 +0300

    organize folders

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
-install:
-	(cd redis-set-get && cargo build)
+start-redis-old:
+	(cd redis-old && docker compose -p redis-old up --build)
+start-shared-memory:
+	(cd shared-memory && docker compose -p shared-memory up --build)
--- shared-memory/write/src/start.py
+++ shared-memory/write/src/start.py
@@ -3,9 +3,12 @@ import time
 import array
 
 def start() -> None:
+    size = 49152000 * 4
+    shm = shared_memory.SharedMemory(create=True, name="my_large_data", size=size)
+    large_data = bytearray(size)
+    large_data[2000] = 5
     start = time.time()
-    shm = shared_memory.SharedMemory(create=True, name="my_large_data", size=49152000 * 4)
-    shm.buf[2000] = 6 # Modify single byte at a time
+    shm.buf[:] = large_data # Modify all data
     print(f"Successfully write \"my_large_data\" of size {len(shm.buf)}, that took {(time.time() - start) * 1000}ms")
     time.sleep(100)
     shm.close()
@@ -14,6 +17,6 @@ def start() -> None:
 # def read() -> None:
 #     start = time.time()
 #     shm = shared_memory.SharedMemory("my_large_data")
-#     assert(shm.buf[2000] == 6)
+#     assert(shm.buf[2000] == 5)
 #     print(f"Successfully read \"my_large_data\" of size {len(shm.buf)}, that took {(time.time() - start) * 1000}ms")
 #     shm.close()
